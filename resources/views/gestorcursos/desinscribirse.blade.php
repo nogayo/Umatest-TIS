cdf4b929dd83d93eba3a524038ec95c05ab0227a
leiderjeil <l.ticlla@clasesumss.com>
2016-07-02 21:12:31 -0400

    mas arreglos de campos para ventanas

--- resources/views/gestorcursos/desinscribirse.blade.php
+++ resources/views/gestorcursos/desinscribirse.blade.php
@@ -8,7 +8,7 @@
 @section('main-content')
 <div class="container">
     <div class="row">
-        <div class="col-md-14 col-md-offset-0" style="margin-right: -0px;">
+        <div class="col-md-14 col-md-offset-0">
             <div class="panel panel-default">
                 <div class="panel-heading">GESTOR DE CURSOS</div>
 
