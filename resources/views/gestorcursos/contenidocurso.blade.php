d9d456276a01782f235118605b46ffa38a28a824
santiagozeballos28 <s.mamani@clasesumss.com>
2016-07-05 02:52:11 -0400

    corregido los errores

--- resources/views/gestorcursos/contenidocurso.blade.php
+++ resources/views/gestorcursos/contenidocurso.blade.php
@@ -33,15 +33,28 @@
             </div>
             <div class="box-body no-padding">
               <ul class="nav nav-pills nav-stacked">
+                   {{-- */$id_user=Auth::id();   
+             /* --}}
+             {{-- */$id_rol=DB::table('role_user')->where('user_id', $id_user)->first();
+                   $id_rol=$id_rol->role_id;    
+             /* --}}
+             {{-- */$name_rol=DB::table('roles')->where('id', $id_rol)->first();
+                    $name_rol=$name_rol->nombre_rol;
+             /* --}}
                 
                 <li><a href="#"><i class="fa fa-file-text-o"></i> Descripcion de la Materia </a></li>
                 <li><a href="#"><i class="fa fa-file-text-o"></i> Documentos y Enlaces </a></li>
                 <li><a href="#"><i class="fa fa-file-text-o"></i> Examenes </a></li>
-                
+                @if($name_rol!='estudiante') 
                 <li><a href="{{ url('gestor_examenes/'.$id_curso.'/examen') }}"><i class="fa fa-file-text-o"></i>crear un nuevo Examen </a></li>
+                @endif
             
                 <li><a href="{{ url('gestor_examenes/'.$id_curso.'/examen/listar/tarea') }}"><i class="fa fa-filter"></i> Mis Tareas </a></li>
+
+             
+                @if($name_rol!='estudiante') 
                 <li><a href="{{ url('gestor_examenes/'.$id_curso.'/examen/crear/tarea') }}"><i class="fa fa-filter"></i> Crear Tarea </a></li>
+                @endif
                 <li><a href="#"><i class="fa fa-envelope-o"></i> Foros </a></li>
 
               </ul>
