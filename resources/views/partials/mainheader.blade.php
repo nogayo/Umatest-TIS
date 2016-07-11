862ce87e70a2cdf0bc4ca5ef31c5754495d3d810
nogayo <nogayo.jpm@gmail.com>
2016-07-11 13:58:27 -0400

    notificaciones al 65%

--- resources/views/partials/mainheader.blade.php
+++ resources/views/partials/mainheader.blade.php
@@ -2,83 +2,88 @@
 <header class="main-header">
 
     <!-- Logo -->
-        <a href="{{ url('/home') }}" class="logo">
+    <a href="{{ url('/home') }}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><img src="{{asset('/img/img_panelPrincipal/home.png')}}"/></span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><img src="{{asset('/img/img_panelPrincipal/home.png')}}"/> </span>
+    </a>
+
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+        <!-- Sidebar toggle <button--></button-->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <span class="sr-only">Toggle navigation</span>
         </a>
 
-          <!-- Header Navbar -->
-          <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle <button--></button-->
-          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-            <span class="sr-only">Toggle navigation</span>
-           </a>
-
-           <div >
+        <div >
 
 
 
 
             <ul class="nav navbar-nav">
 
-             <li class="active"><a href="{{ url('/resenia_historica')}}">Reseña histórica<span class="sr-only">(current)</span></a></li>
-             <li class="active"><a href="{{ url('/mision') }}">Misión<span class="sr-only">(current)</span></a></li>
-             <li class="active"><a href="{{ url('/vision') }}">Visión<span class="sr-only">(current)</span></a></li>
-            
-
-             <li class="active"><a href="{{ url('/contactos') }}">Contactos<span class="sr-only">(current)</span></a></li>
-             <li class="active"><a href="{{ url('/ayuda') }}">Ayuda<span class="sr-only">(current)</span></a></li>
-             </ul>
-
-          </div>
+                <li class="active"><a href="{{ url('/resenia_historica')}}">Reseña histórica<span class="sr-only">(current)</span></a></li>
+                <li class="active"><a href="{{ url('/mision') }}">Misión<span class="sr-only">(current)</span></a></li>
+                <li class="active"><a href="{{ url('/vision') }}">Visión<span class="sr-only">(current)</span></a></li>
 
 
+                <li class="active"><a href="{{ url('/contactos') }}">Contactos<span class="sr-only">(current)</span></a></li>
+                <li class="active"><a href="{{ url('/ayuda') }}">Ayuda<span class="sr-only">(current)</span></a></li>
+            </ul>
+
+        </div>
 
 
-         
 
-              <!-- Navbar Right Menu -->
-        <div class="navbar-custom-menu">
+
+
+
+        <!-- Navbar Right Menu -->
+        <div data-id="{{ Auth::id() }}" class="navbar-custom-menu">
             <ul class="nav navbar-nav">
                 <!-- Notifications: style can be found in dropdown.less -->
                 <li class="dropdown notifications-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-bell-o"></i>
-                        <span class="label label-warning">
+                    <a  href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i  class="fa fa-bell-o"></i>
 
-                             {{--*/ $id_user=Auth::id();
-
+                        {{--*/ $id_user=Auth::id();
 
 
-                                $num_users=DB::table('curso_inscritos')->where('user_id', $id_user)->get();
 
-                                $num_users=count($num_users);
+                              $num_users=DB::table('notificacions')->where('id_user', $id_user)->get();
 
-                             /* --}}
+                              $num_users=count($num_users);
 
+                           /* --}}
 
+                        @if ($num_users != 0)
+
+                            <span class="label label-warning">
                             {{
                                 $num_users
-
                              }}
-                        </span>
+                            </span>
+
+                        @endif
+
+
+
                     </a>
                     <ul class="dropdown-menu">
-                        <li class="header">You have 10 notifications</li>
+                        <li class="header">Usted tiene {{ $num_users  }} notification(es)</li>
                         <li>
                             <!-- inner menu: contains the actual data -->
                             <ul class="menu">
                                 <li>
                                     <a href="#">
-                                        <i class="ion ion-ios-people info"></i> Notification title
+                                        <i class="ion ion-ios-people info"></i> Titulo Notificacion
                                     </a>
                                 </li>
                                 ...
                             </ul>
                         </li>
-                        <li class="footer"><a href="#">View all</a></li>
+                        <li class="footer"><a href="#">Ver Todos</a></li>
                     </ul>
                 </li>
                 <!-- User Account Menu -->
@@ -104,23 +109,7 @@
                         <li class="user-footer">
                             <div class="pull-left">
                                 {{-- */$id_user=Auth::id(); /* --}}
-
-                                {{-- */$id_user=Auth::id();   
-                                /* --}}
-                                {{-- */$id_rol=DB::table('role_user')->where('user_id', $id_user)->first();
-                                        $id_rol=$id_rol->role_id;    
-                                /* --}}
-                                {{-- */$name_rol=DB::table('roles')->where('id', $id_rol)->first();
-                                        $name_rol=$name_rol->nombre_rol;
-                                /* --}}
-                                @if($name_rol=='administrador') 
-                                    <a href="{{ url('/admin/administrador/' . $id_user . '/edit') }}" class="btn btn-default btn-flat">Perfil</a>
-
-                                @else
-                                        <a href="{{ url('/admin/users/' . $id_user . '/edit') }}" class="btn btn-default btn-flat">Perfil</a>  
-                                @endif
-
-                                
+                                <a href="{{ url('/admin/users/' . $id_user . '/edit') }}" class="btn btn-default btn-flat">Perfil</a>
                             </div>
 
                             <div class="pull-right">
